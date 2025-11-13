82745dbc77bd7b8801fda00fd9d17d7b1ea524f0
Tiziano Zingales <tiziano.zingales@gmail.com>
2025-11-13 13:19:56 +0100

    added solution for transit class and initial structure for artificial intelligence tasks

--- src/daneel/__main__.py
+++ src/daneel/__main__.py
@@ -17,6 +17,15 @@ def main():
     )
 
     parser.add_argument(
+        "-t",
+        "--transit",
+        dest="transit",
+        required=False,
+        help="Plots transit lightcurve from yaml file",
+        action="store_true",
+    )
+
+    parser.add_argument(
         "-d",
         "--detect",
         dest="detect",
@@ -24,6 +33,7 @@ def main():
         help="Initialise detection algorithms for Exoplanets",
         action="store_true",
     )
+    
 
     parser.add_argument(
         "-a",
@@ -51,9 +61,12 @@ def main():
 
     input_pars = Parameters(args.input_file).params
 
-    if args.detect:
+    if args.transit:
+        transit = TransitModel(input_pars['transit'])
+        transit.plot_light_curve()
+    elif args.detect:
         pass
-    if args.atmosphere:
+    elif args.atmosphere:
         pass
     if args.transit:
     	transit = Transit(input_pars)
---
+++ src/daneel/detection/classifiers.py
@@ -0,0 +1,5 @@
+class RandomForestClassifier:
+    pass
+
+class CNNClassifier:
+    pass
